0472c28b246100c6cab9fd375df5fcb99b61e6c4
HopSoft-Tech <ndukelex4@gmail.com>
2024-04-06 04:08:53 +0100

    Made changes to the app.js, styles.css and index.html files

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
+// IIFE (Immediately Invoked Function Expressions)
 (function () {
   document.querySelectorAll(".control").forEach((button) => {
     button.addEventListener("click", function () {
       document.querySelector(".active-btn").classList.remove("active-btn");
       this.classList.add("active-btn");
       document.querySelector(".active").classList.remove("active");
-      document.getElementById(button.dataset.id).classList.add("active");
+      document
+        .getElementById(button.getAttribute("data-id"))
+        .classList.add("active");
     });
   });
   document.querySelector(".theme-btn").addEventListener("click", () => {
@@ -22,7 +25,7 @@
   });
 })();
 
-document.querySelector(".contact-form").addEventListener("submit", (e) => { 
+document.querySelector(".contact-form").addEventListener("submit", (e) => {
   const name = document.getElementById("name").value.trim();
   const email = document.getElementById("email").value.trim();
   const subject = document.getElementById("subject").value.trim();
@@ -30,13 +33,11 @@ document.querySelector(".contact-form").addEventListener("submit", (e) => {
   const hiddenInput = document.getElementById("hidden-input");
 
   // Check if all fields are filled out
-  if(!name || !email || !subject || !message) {
+  if (!name || !email || !subject || !message) {
     e.preventDefault();
     alert("Please fill out all fields");
     return;
   }
 
   hiddenInput.value = subject;
-
 });
-
